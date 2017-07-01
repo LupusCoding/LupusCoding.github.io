7f30dce076a8d53a8ce468c8d5e9dc3d5c698927
LupusCoding <LupusCoding@gmail.com>
2017-07-01 13:24:25 +0200

    Added gallery.js

---
+++ js/gallery.js
@@ -0,0 +1,35 @@
+function getLists() {
+    setTimeout(function() {
+        if(documents != null) {
+            getImageGalleries(0);
+    		var galleryList   = document.getElementById('img_list').children;
+    		var galleryFullWidth = 0;
+    		for(var i=0; i<galleryList.length; i++) {
+    			galleryList[i].getElementsByTagName('a')[0].addEventListener('click', 
+    				function(i) { 
+	    				loadGallery(this.getAttribute('gid')); 
+	    			} 
+    			);
+    			galleryFullWidth += galleryList[i].offsetWidth;
+    		}
+    		console.log(galleryFullWidth);
+    		var galleryContainer = document.getElementById('img_list');
+    		if(galleryFullWidth > galleryContainer.offsetWidth) {
+    			galleryContainer.parent.style.overflow = 'hidden';
+    			galleryContainer.parent.style.overflow = 'hidden';
+    			galleryContainer.style.width = galleryFullWidth;
+    		}
+        } else {
+            getLists();
+        }
+    },500);
+}
+
+// we take the resources first, so we can use them at DOMContentLoaded state
+getResources();
+document.addEventListener('DOMContentLoaded', function() {
+    getLists();
+
+    document.getElementsByClassName('overlay-bg')[0].addEventListener('click', function() { closeOverlay(); } );
+    document.getElementById('overlay_close').addEventListener('click', function() { closeOverlay(); } );
+});
